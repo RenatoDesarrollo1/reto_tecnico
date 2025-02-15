737717ae1f9d0798000e3d3368336fa37847f097
RenatoArenas <renato.arenas2012@gmail.com>
2025-02-15 15:52:11 -0500

    Modelos extienden de BaseModel y error en authorize en ProductUpdateRequest

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Product extends Model
+class Product extends BaseModel
 {
     use HasFactory, SoftDeletes;
 
--- app/Models/SaleDetail.php
+++ app/Models/SaleDetail.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class SaleDetail extends Model
+class SaleDetail extends BaseModel
 {
     use HasFactory, SoftDeletes;
     protected $fillable = [
